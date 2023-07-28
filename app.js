6df8d11aeac47bcfc0058322748cc800881ff9bd
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-07-28 01:22:11 +0000

    [Finish] javascript 6章

--- app.js
+++ app.js
@@ -34,12 +34,42 @@
 //});
 
 //確認問題
+//$(function(){
+//    $('.box1').slideDown(function(){
+ //       $('.box1').css({
+ //           'background-color': '#0000FF',
+ //           'width': '200px',
+ //           'height': '100px'
+ //       }).slideUp();
+ //   });
+//});
+
+//マウスオーバー/マウスアウト
+//$(function(){
+//    $('.box1').mouseover(function(){
+//        $('.box1').css({'background-color': '#0000FF'});
+//    });
+//     $('.box1').mouseout(function(){
+//        $('.box1').css({'background-color': '#FF0000'});
+//    });
+//});
+
+//class属性
+//$(function(){
+//   $('.box1').mouseover(function(){
+//       $('.box1').addClass('box1-ext');
+//   }) ;
+//   $('.box1').mouseout(function(){
+//       $('.box1').removeClass('box1-ext');
+//   }) ;
+//});
+
+//マウスクリック
 $(function(){
-    $('.box1').slideDown(function(){
-        $('.box1').css({
-            'background-color': '#0000FF',
-            'width': '200px',
-            'height': '100px'
-        }).slideUp();
-    });
+   $('.box1').on('click', function(){
+       $('.box1').addClass('box1-ext');
+   }) ;
+   $('.box1').mouseout(function(){
+       $('.box1').removeClass('box1-ext');
+   }) ;
 });
